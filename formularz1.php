c6bdac6caddf4bcb62c95e47b0ce3f06ca252790
AxisFray <113852846+AxisFray@users.noreply.github.com>
2024-01-24 07:43:37 +0100

    Add files via upload

---
+++ formularz1.php
@@ -0,0 +1,11 @@
+<?php
+echo "Imie - ".$_GET['f1']."<br>";
+echo "Nazwisko - ".$_GET['f2']."<br>";
+echo "Wiek - ".$_GET['f3']."<br>";
+echo "Email - ".$_GET['f4']."<br>";
+echo "Wykształcenie - ".$_GET['wyk']."<br>";
+echo "Zgoda - ".$_GET['zgoda']."<br>";
+
+
+
+?>
