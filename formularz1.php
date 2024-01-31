20a16ef6b58e2426923b00d98d4546053d01fb62
AxisFray <113852846+AxisFray@users.noreply.github.com>
2024-01-31 08:42:12 +0100

    Add files via upload

--- formularz1.php
+++ formularz1.php
@@ -3,13 +3,23 @@ echo "Imie - ".$_POST['f1']."<br>";
 echo "Nazwisko - ".$_POST['f2']."<br>";
 echo "Wiek - ".$_POST['f3']."<br>";
 echo "Email - ".$_POST['f4']."<br>";
-echo "Wykształcenie - ".$_POST['wyk']."<br>";
-echo "Zgoda - ".$_POST['zgoda']."<br>";
-//echo "Jezyk - ".$_POST['jezyk']."<br>";
-foreach($_POST['jezyk'] as $j)
-{
-    echo "<li>".$j."<li><br>";
-}
 
+if(isSet($_POST["wyk"])){
+echo "Wykształcenie - ".$_POST['wyk']."<br>";}
+else{
+    echo "<p style='color:red'>Nie zaznaczyles poziomu swojego wyksztalcenia</p>";
+}
+//echo "Jezyk - ".$_POST['jezyk']."<br>";
+
+if(isSet($_POST["jezyk"][0])){
+    foreach($_POST['jezyk'] as $j)
+{
+    echo "<li>".$j."<li><br>";}
+}
+else{
+        echo "<p style='color:red'>Nie zaznaczyles jezyka</p>";
+    }
+
+echo $_FILES['plik1']['tmp_name']." ". $_FILES['plik1']['size']." ". $_FILES['plik1']['type'];
 
 ?>
