bda9ece8a9d71d8e53e971b9e8970831e106fec9
AxisFray <113852846+AxisFray@users.noreply.github.com>
2024-03-20 10:06:55 +0100

    Add files via upload

---
+++ tasks/formularz1.php
@@ -0,0 +1,27 @@
+<?php
+echo "Imie - ".$_POST['f1']."<br>";
+echo "Nazwisko - ".$_POST['f2']."<br>";
+echo "Wiek - ".$_POST['f3']."<br>";
+echo "Email - ".$_POST['f4']."<br>";
+
+if(isSet($_POST["wyk"])){
+echo "Wykształcenie - ".$_POST['wyk']."<br>";}
+else{
+    echo "<p style='color:red'>Nie zaznaczyles poziomu swojego wyksztalcenia</p>";
+}
+//echo "Jezyk - ".$_POST['jezyk']."<br>";
+
+if(isSet($_POST["jezyk"][0])){
+    foreach($_POST['jezyk'] as $j)
+{
+    echo "<li>".$j."<li><br>";}
+}
+else{
+        echo "<p style='color:red'>Nie zaznaczyles jezyka</p>";
+    }
+
+echo $_FILES['plik1']['tmp_name']." , ". $_FILES['plik1']['size']." , ". $_FILES['plik1']['type'];
+//move_uploaded_file()
+//is_uploaded_file()
+?>
+
