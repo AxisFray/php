617ebb5cb817d9b3e999e8666458a0a953b653a1
AxisFray <113852846+AxisFray@users.noreply.github.com>
2024-05-21 20:56:06 +0200

    Add files via upload

--- tasks/projekt 30.04.24/ksiegarnia_internetowa.sql
+++ tasks/projekt 30.04.24/ksiegarnia_internetowa.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 31 Sty 2018, 10:36
--- Wersja serwera: 10.1.19-MariaDB
--- Wersja PHP: 5.6.28
+-- Generation Time: Maj 21, 2024 at 08:54 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,59 +18,79 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `ksiegarnia_internetowa2`
+-- Database: `ksiegarnia_internetowa`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `klient`
+-- Struktura tabeli dla tabeli `klienci`
 --
 
-CREATE TABLE `klient` (
-  `Id_klienta` int(11) NOT NULL,
-  `Nazwisko` varchar(30) COLLATE utf8_polish_ci NOT NULL,
-  `Imie` varchar(15) COLLATE utf8_polish_ci NOT NULL,
-  `Kod_pocztowy` text COLLATE utf8_polish_ci NOT NULL,
-  `Miejscowosc` varchar(20) COLLATE utf8_polish_ci NOT NULL,
-  `Ulica` varchar(15) COLLATE utf8_polish_ci NOT NULL,
-  `Nr_domu` text COLLATE utf8_polish_ci NOT NULL,
-  `PESEL` int(11) NOT NULL,
-  `Telefon` int(11) NOT NULL,
-  `AdresEmail` text COLLATE utf8_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+CREATE TABLE `klienci` (
+  `idKlienta` int(11) NOT NULL,
+  `login` varchar(40) DEFAULT NULL,
+  `haslo` varchar(30) DEFAULT NULL,
+  `imie` varchar(30) DEFAULT NULL,
+  `nazwisko` varchar(30) DEFAULT NULL,
+  `telefon` char(9) DEFAULT NULL,
+  `miejscowosc` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Zrzut danych tabeli `klient`
+-- Struktura tabeli dla tabeli `ksiazki`
 --
 
-INSERT INTO `klient` (`Id_klienta`, `Nazwisko`, `Imie`, `Kod_pocztowy`, `Miejscowosc`, `Ulica`, `Nr_domu`, `PESEL`, `Telefon`, `AdresEmail`) VALUES
-(1, 'Jorek', 'Ewa', '33-100', 'Tarnów', 'Kościuszki', '15/9', 0, 451421512, 'ewa@02.pl'),
-(2, 'Kicior', 'Marek', '33-100', 'Tarnów', 'Lwowska', '12', 0, 215121215, 'marek@poczta.pl'),
-(3, '', '', '', '', '', '', 0, 0, ''),
-(4, '', '', '', '', '', '', 0, 0, ''),
-(5, 'Nosalski', 'Egon', '25-124', 'Robaki', 'Kornika', '56', 0, 542121212, 'kornik1@nowy.com'),
-(6, 'Żabka', 'Kacper', '78-512', 'Londonek', 'Korce', '325/12', 0, 741741741, 'Korce@dkdal.com');
+CREATE TABLE `ksiazki` (
+  `idKsiazki` int(11) NOT NULL,
+  `tytul` varchar(35) DEFAULT NULL,
+  `cena` float DEFAULT NULL,
+  `autor` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ksiazki`
+--
+
+INSERT INTO `ksiazki` (`idKsiazki`, `tytul`, `cena`, `autor`) VALUES
+(1, 'Pan Tadeusz', 12.5, 'Adam Mickiewicz'),
+(2, 'Tchnienie ruppali', 40.8, 'Anna Kosciolek');
 
 --
 -- Indeksy dla zrzutów tabel
 --
 
 --
--- Indexes for table `klient`
+-- Indeksy dla tabeli `klienci`
 --
-ALTER TABLE `klient`
-  ADD PRIMARY KEY (`Id_klienta`);
+ALTER TABLE `klienci`
+  ADD PRIMARY KEY (`idKlienta`);
+
+--
+-- Indeksy dla tabeli `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  ADD PRIMARY KEY (`idKsiazki`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `klient`
+-- AUTO_INCREMENT for table `klienci`
 --
-ALTER TABLE `klient`
-  MODIFY `Id_klienta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `klienci`
+  MODIFY `idKlienta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  MODIFY `idKsiazki` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
