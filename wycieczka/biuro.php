7ed040eee33b79b821bc6f00a34f53a8469e066c
AxisFray <113852846+AxisFray@users.noreply.github.com>
2025-02-27 12:50:35 +0100

    Add files via upload

---
+++ wycieczka/biuro.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="styl4.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<header>
+<h1>WITAMY W BIURZE PODRÓŻY</h1>
+
+</header>
+
+<aside>
+    <h3 style="text-align:center">ARCHIWUM WYCIECZEK</h3>
+<div id="dane">
+<ol type="1">
+    <li>Francja,Paryż, cena: 1350</li>
+	<li>Francja,Paryż, cena: 1350</li>
+	<li>Francja,Paryż, cena: 1350</li>
+</ol>
+</div>
+
+</aside>
+
+<div id="left">
+    <h3>NAJTANIEJ</h3>
+    </div>
+<div id="center">
+    <h3>TU BYLIŚMY</h3>
+    
+    <table>
+    
+    <tr><td><img src="1.jpg"></td><td><img src="2.jpg"></td><td><img src="3.jpg"></td></tr>
+    <tr><td><img src="4.jpg"></td><td><img src="5.jpg"></td><td><img src="6.jpg"></td></tr>
+    <tr><td><img src="7.jpg"></td><td><img src="8.jpg"></td><td><img src="9.jpg"></td></tr>
+    
+    </table>
+    
+    
+    </div>
+<div id="right">
+    <h3>SKONTAKTUJ SIĘ</h3>
+    <a href="wycieczki@wycieczki.pl" >napisz do nas</a>
+    <p>telefon: 555666777</p>
+    </div>
+    
+    
+    <footer>
+    Stronę wykonał: 3980249;
+    
+    </footer>
+    
+    
+</body>
+
+
+
+</html>
